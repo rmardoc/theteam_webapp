5a0ee35091ee890c661fe6352a711559c5f42eaa
Riccardo Giannico <richard.mardoc@gmail.com>
2025-03-29 23:58:48 +0100

    fix heroku

--- settings.py
+++ settings.py
@@ -1,6 +1,11 @@
 import streamlit as st
+import os
+#import psycopg2
+
+
 
 def init():
     global conn
     #app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL') or "postgresql://postgres:password@localhost:5432/postgres"
-    conn = st.connection("herokupostgresql", url = "env:DATABASE_URL", type="sql")
+    DATABASE_URL = os.environ['DATABASE_URL']
+    conn = st.connection("herokupostgresql", url = DATABASE_URL, type="sql")
