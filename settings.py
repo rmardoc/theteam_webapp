c99496d5a73ee380200f02b51c936a388d4145fd
Riccardo Giannico <richard.mardoc@gmail.com>
2025-03-29 23:39:35 +0100

    add heroku postgresql info

--- settings.py
+++ settings.py
@@ -2,4 +2,5 @@ import streamlit as st
 
 def init():
     global conn
-    conn = st.connection("postgresql", type="sql")
+    #app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL') or "postgresql://postgres:password@localhost:5432/postgres"
+    conn = st.connection("herokupostgresql", url = "env:DATABASE_URL", type="sql")
